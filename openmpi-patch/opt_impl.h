59e1af30a5b6a7148fee15ea2b31464eb5a05a60
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2022-02-23 19:18:24 +0100

    added header for easier testing

---
+++ openmpi-patch/opt_impl.h
@@ -0,0 +1,11 @@
+#ifndef OPT_IMPL_H
+#define OPT_IMPL_H
+
+//#include "openmpi-patch/low_level.h"
+//#include "openmpi-patch/low-level.c"
+
+#include "low_level.h"
+
+#include "low-level.c"
+
+#endif /* OPT_IMPL_H */
