88a5abf94d6a9942bd12cc98e06ef5ad31801633
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2022-02-24 11:51:44 +0100

    added instructions to build OpenMPI with patch

--- openmpi-patch/one-sided-persistent.c
+++ openmpi-patch/one-sided-persistent.c
@@ -1,22 +1,67 @@
-#include "low_level.h"
 
-#define INIT_MSG_TAG 1337
+#include <mpi.h>
+#include <ucp/api/ucp.h>
 
-/*
- #include <mpi.h>
- #include <ucp/api/ucp.h>
+// why does other header miss this include?
+#include <stdbool.h>
 
- // why does other header miss this include?
- #include <stdbool.h>
+#include "ompi/mca/osc/base/base.h"
+#include "ompi/mca/osc/base/osc_base_obj_convert.h"
+#include "ompi/mca/osc/osc.h"
+#include "opal/mca/common/ucx/common_ucx.h"
 
- #include "ompi/mca/osc/base/base.h"
- #include "ompi/mca/osc/base/osc_base_obj_convert.h"
- #include "ompi/mca/osc/osc.h"
- #include "opal/mca/common/ucx/common_ucx.h"
+#include "ompi/mca/osc/ucx/osc_ucx.h"
+#include "ompi/mca/osc/ucx/osc_ucx_request.h"
 
- #include "ompi/mca/osc/ucx/osc_ucx.h"
- #include "ompi/mca/osc/ucx/osc_ucx_request.h"
- */
+// config :
+// TODO is there an openmpi internal value for tag UB
+// we can set tag UB to TAG_UB/2
+#define COMM_BEGIN_TAG 32767
+// count is not part of the message envelope
+#define RDMA_SPIN_WAIT_THRESHOLD 32
+
+#define STATISTIC_PRINTING
+
+//end config
+
+#define RECV_REQUEST_TYPE 1
+#define SEND_REQUEST_TYPE 2
+#define SEND_REQUEST_TYPE_SEARCH_FOR_RDMA_CONNECTION 3
+#define RECV_REQUEST_TYPE_SEARCH_FOR_RDMA_CONNECTION 4
+#define SEND_REQUEST_TYPE_USE_FALLBACK 5
+#define Recv_REQUEST_TYPE_USE_FALLBACK 6
+
+struct mpiopt_request {
+  // this way it it can be used as a normal request ptr as well
+  struct ompi_request_t original_request;
+  int flag;
+  int flag_buffer;
+  uint64_t remote_data_addr;
+  uint64_t remote_flag_addr;
+  ucp_rkey_h remote_data_rkey;
+  ucp_rkey_h remote_flag_rkey;
+  void *buf;
+  size_t size;
+  // initialized locally
+  void *ucx_request_data_transfer;
+  void *ucx_request_flag_transfer;
+  int operation_number;
+  int type;
+  ucp_mem_h mem_handle_data;
+  ucp_mem_h mem_handle_flag;
+  ucp_ep_h
+      ep; // save used endpoint, so we dont have to look it up over and over
+  // necessary for backup in case no other persistent op matches
+  MPI_Request backup_request;
+  int tag;
+  int dest;
+  MPI_Comm comm;
+  MPI_Request rdma_exchange_request;
+  MPI_Request rdma_exchange_request_send;
+  // struct mpiopt_request* rdma_exchange_buffer;
+};
+typedef struct mpiopt_request MPIOPT_Request;
+
 
 MPI_Win global_comm_win;
 int dummy_int = 0;
@@ -51,7 +96,6 @@ static void wait_for_completion_blocking(void *request) {
   ucp_request_free(request);
 }
 
-//#define STATISTIC_PRINTING
 
 static void acknowlege_Request_free(MPIOPT_Request *request) {
   // wait for any outstanding RDMA Operation (i.e. transfer of flag that comm
