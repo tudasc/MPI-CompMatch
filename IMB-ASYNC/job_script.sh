e0cc8d5b60cbb96e93c0fa09fcc47ac230e83990
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2022-05-11 16:34:53 +0200

    jobscript for large scale measurement

--- IMB-ASYNC/job_script.sh
+++ IMB-ASYNC/job_script.sh
@@ -5,25 +5,26 @@
 
 #SBATCH --mem-per-cpu=10
 #same as -t
-#SBATCH --time 00:10:00
+#SBATCH --time 08:10:00
 
 #same as -c
 #SBATCH --cpus-per-task 96
-# opne node per process
+# one node per process
 
 ###SBATCH --array 0-1
-###SBATCH --array 0-11
+###SBATCH --array 0-10
 ###SBATCH --array 0-1
 
 #same as -j
 #SBATCH --job-name MPI-ASYNC-BENCHMARK
 #same as -o
-##SBATCH --output output/job_%a.out
-#SBATCH --output job.out
+###SBATCH --output output/job_%a.out
+#SBATCH --output job_%j.out
 #same as -e
 #SBATCH --error err/job_%a.err
 
 #here the jobscript starts
+
 
 srun hostname
 
@@ -36,20 +37,31 @@ export OMPI_MCA_pml=ucx
 export UCX_WARN_UNUSED_ENV_VARS=n
 export UCX_UNIFIED_MODE=y
 
+LEN="-len 4,8,32,512,1024,4096,16384,65536,262144,1048576,4194304,16777216 -ncycles 10000 -datatype char "
+LEN2="-len 4,8,32,512,1024,4096,16384,65536,262144,1048576,4194304,16777216 -ncycles 100 -datatype char "
+
 readarray -t PARAMS < /home/tj75qeje/mpi-comp-match/IMB-ASYNC/parameters.txt
 
-#PARAM=${PARAMS[$SLURM_ARRAY_TASK_ID]}
-PARAM=${PARAMS[0]}
+
+echo "Original"
+srun ./IMB-ASYNC_orig calc_calibration
+echo "Compiler Assisted"
+srun ./IMB-ASYNC calc_calibration
+
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./src_cpp/ASYNC/thirdparty/lib/
 
-echo "Original $PARAM"
-srun ./IMB-ASYNC_orig calc_calibration
-srun ./IMB-ASYNC_orig async_persistentpt2pt -cper10usec 64 -workload calc -nwarmup 100 -thread_level single $PARAM
+for PARAM in "${PARAMS[@]}" ; do
 
-echo "Compiler Assisted $PARAM"
-srun ./IMB-ASYNC calc_calibration
-srun ./IMB-ASYNC async_persistentpt2pt -cper10usec 64 -workload calc -nwarmup 100 -thread_level single $PARAM
+srun ./IMB-ASYNC_orig async_persistentpt2pt -cper10usec 64 -workload calc -nwarmup 100 -thread_level single $LEN -calctime $PARAM -output output/orig_calctime_${PARAM}.$SLURM_JOB_ID.yaml >& /dev/null
 
+srun ./IMB-ASYNC async_persistentpt2pt -cper10usec 64 -workload calc -nwarmup 100 -thread_level single $LEN -calctime $PARAM -output output/modified_calctime_${PARAM}.$SLURM_JOB_ID.yaml >& /dev/null
+
+srun ./IMB-ASYNC_orig async_persistentpt2pt -cper10usec 64 -workload calc -nwarmup 0 -thread_level single $LEN2 -calctime $PARAM -output output/orig_NOwarmup_calctime_${PARAM}.$SLURM_JOB_ID.yaml >& /dev/null
+
+srun ./IMB-ASYNC async_persistentpt2pt -cper10usec 64 -workload calc -nwarmup 0 -thread_level single $LEN2 -calctime $PARAM -output output/modified_NOwarmup_calctime_${PARAM}.$SLURM_JOB_ID.yaml >& /dev/null
+
+
+done
 echo "done"
 
