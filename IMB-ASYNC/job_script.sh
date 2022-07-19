7ed05f0a69d690b68ede62591dc1349dbff89469
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2022-07-19 15:25:15 +0200

    removed more unused files

--- IMB-ASYNC/job_script.sh
+++ IMB-ASYNC/job_script.sh
@@ -5,32 +5,40 @@
 
 #SBATCH --mem-per-cpu=3800
 #same as -t
-#SBATCH --time 00:30:00
+###SBATCH --time 00:30:00
+# approx half a minute per benchmark, 4 benchmarks * 28 parameters
+# approx 2h
+#SBATCH --time 00:05:00
 
 #same as -c
 #SBATCH --cpus-per-task 96
 # one node per process
 
 ###SBATCH --array 0-1
-#SBATCH --array 1-100
-###SBATCH --array 0-1
+###SBATCH --array 1-20
+# 1 run for each parameter
+#SBATCH --array 1-240
+###SBATCH --array 0-10
 
 #same as -j
 #SBATCH --job-name MPI-ASYNC-BENCHMARK
 #same as -o
-###SBATCH --output output/job_%a.out
+#SBATCH --output output/job_%a.out
 ## The real output will be saved into yml files
-#SBATCH --output /dev/null
-#same as -e
-#SBATCH --error err/job_%a.err
+####SBATCH --output /dev/null
 
+# config
+OUTPATH=/work/scratch/tj75qeje/mpi-comp-match/output/measurement_$SLURM_NPROCS
+NUM_PARAMS=12
+
+TIMEOUT_CMD="/usr/bin/timeout -k 120 120"
 #here the jobscript starts
-
-OUTPATH=/work/scratch/tj75qeje/mpi-comp-match/output
-
 #srun hostname
 
-ml gcc/8.3.1 openmpi/test hwloc/2.5.0 clang/11.1.0
+ml purge
+ml gcc/8.3.1
+ml hwloc/2.5.0 clang/11.1.0
+ml openucx/1.12.0
 
 #export OMPI_MCA_opal_warn_on_missing_libcuda=0
 #export OMPI_MCA_opal_common_ucx_opal_mem_hooks=1
@@ -39,41 +47,36 @@ export OMPI_MCA_pml=ucx
 export UCX_WARN_UNUSED_ENV_VARS=n
 export UCX_UNIFIED_MODE=y
 
-# Fiull configuration
-# after a warmup period
-#LEN="-len 4,8,32,512,1024,4096,16384,65536,262144,1048576,4194304,16777216 -ncycles 256 -nwarmup 64  -datatype char "
-# only the warmup phase
-#LEN2="-len 4,8,32,512,1024,4096,16384,65536,262144,1048576,4194304,16777216 -ncycles 64 -nwarmup 0 -datatype char "
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/tj75qeje/mpi-comp-match/IMB-ASYNC/src_cpp/ASYNC/thirdparty/lib/
 
-# limited number of buffer lengths
-# after a warmup period
-LEN="-len 8,1024,16384,65536,262144,1048576,4194304,16777216 -ncycles 256 -nwarmup 64  -datatype char "
-# only the warmup phase
-LEN2="-len 8,1024,16384,65536,262144,1048576,4194304,16777216 -ncycles 64 -nwarmup 0 -datatype char "
+mkdir -p $OUTPATH
 
-#readarray -t PARAMS < /home/tj75qeje/mpi-comp-match/IMB-ASYNC/parameters.txt
+MOD=$(( SLURM_ARRAY_TASK_ID % 4 ))
+MOD_PARAM=$(( ((SLURM_ARRAY_TASK_ID / 4 ) % NUM_PARAMS ) + 1 ))
 
 
-#echo "Original"
-#srun ./IMB-ASYNC_orig calc_calibration
-#echo "Compiler Assisted"
-#srun ./IMB-ASYNC calc_calibration
+PARAM=$(sed "${MOD_PARAM}q;d" /home/tj75qeje/mpi-comp-match/IMB-ASYNC/parameters.txt)
+# get calctime out of param, as calctime will not be included inside the yml, we need to include it in the filename
+CALCTIME=$(echo $PARAM | cut -d' ' -f2)
+
+if [[ "$MOD" -eq 0 ]]; then
+ml openmpi/normal
+
+$TIMEOUT_CMD srun ./IMB-ASYNC_orig async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single -datatype char -ncycles 64 -nwarmup 10 $PARAM -output $OUTPATH/normal_calctime_${CALCTIME}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.$I.yaml >& /dev/null
+elif [[ "$MOD" -eq 1 ]]; then
+
+ml openmpi/rendevouz1
+
+$TIMEOUT_CMD srun ./IMB-ASYNC async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single -datatype char -ncycles 64 -nwarmup 10 $PARAM -output $OUTPATH/rendevouz1_calctime_${CALCTIME}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.$I.yaml >& /dev/null
+elif [[ "$MOD" -eq 2 ]]; then
+ml openmpi/rendevouz2
+
+$TIMEOUT_CMD srun ./IMB-ASYNC async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single -datatype char -ncycles 64 -nwarmup 10 $PARAM -output $OUTPATH/rendevouz2_calctime_${CALCTIME}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.$I.yaml >& /dev/null
+else
+ml openmpi/eager
+
+$TIMEOUT_CMD srun ./IMB-ASYNC async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single -datatype char -ncycles 64 -nwarmup 10 $PARAM -output $OUTPATH/eager_calctime_${CALCTIME}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.$I.yaml >& /dev/null
+fi
 
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./src_cpp/ASYNC/thirdparty/lib/
-
-# random parameterreihenfolge, im moment haben wir immer bei einem bestimmten wert einen zu hohen overhead (slrum control deamon interrupting the measurement?)
-for PARAM in $(shuf /home/tj75qeje/mpi-comp-match/IMB-ASYNC/parameters.txt ) ; do
-
-srun ./IMB-ASYNC_orig async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single $LEN -calctime $PARAM -output $OUTPATH/orig_calctime_${PARAM}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.yaml >& /dev/null
-
-srun ./IMB-ASYNC async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single $LEN -calctime $PARAM -output $OUTPATH/modified_calctime_${PARAM}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.yaml >& /dev/null
-
-srun ./IMB-ASYNC_orig async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single $LEN2 -calctime $PARAM -output $OUTPATH/orig_NOwarmup_calctime_${PARAM}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.yaml >& /dev/null
-
-srun ./IMB-ASYNC async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single $LEN2 -calctime $PARAM -output $OUTPATH/modified_NOwarmup_calctime_${PARAM}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.yaml >& /dev/null
-
-
-done
-echo "done"
 
