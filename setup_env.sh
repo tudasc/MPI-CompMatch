0cb4b2a6ee8d9daa0d248b26ac3947fb1241d8ca
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2022-07-19 15:12:02 +0200

    current setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-ml openmpi/test hwloc/2.5.0 clang/11.1.0
+ml gcc/8.3
+
+ml openmpi/test ucx/1.12.1 hwloc/2.5.0 clang/11.1.0
+
+# use cluster version of ucx for production run
+ml unload ucx
+ml openucx/1.12.0
+ml openmpi/rendevouz2
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/tj75qeje/mpi-comp-match/IMB-ASYNC/src_cpp/ASYNC/thirdparty/lib/
 
 export OMPI_MCA_opal_warn_on_missing_libcuda=0
 export OMPI_MCA_opal_common_ucx_opal_mem_hooks=1
