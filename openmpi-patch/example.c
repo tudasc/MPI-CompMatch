a17c77f1e3da6bcc1fd983397c2c196168b3fbde
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-01-19 10:26:35 +0100

    fix makefile to build example

--- openmpi-patch/example.c
+++ openmpi-patch/example.c
@@ -84,7 +84,7 @@ void use_self_implemented_comm() {
   double *work_buffer = malloc(N * sizeof(double));
   work_buffer[N - 1] = 0.6;
 
-  MPIOPT_Request req;
+  MPI_Request req;
 
   if (rank == 1) {
 
@@ -101,7 +101,7 @@ void use_self_implemented_comm() {
     }
   } else {
 
-    MPIOPT_Recv_init(buffer, sizeof(int) * N, MPI_BYTE, 1, 42, MPI_COMM_WORLD,
+    MPIOPT_Recv_init(buffer, sizeof(int) * N,MPI_BYTE, 1, 42, MPI_COMM_WORLD,
                      &req);
     for (int n = 0; n < NUM_ITERS; ++n) {
 
